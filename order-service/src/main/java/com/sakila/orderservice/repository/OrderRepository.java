f37bd0b63d558f38d9e58206f3b4c1d970bf1a74
Madzilla <cryptomtz@hotmail.com>
2023-07-06 17:28:17 -0600

    Refactorization of the structure of microservices

--- order-service/src/main/java/com/sakila/orderservice/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sakila.orderservice.repository;
-
-import com.sakila.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- product-service/src/main/java/com/sakila/productservice/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sakila.productservice.repository;
-
-import com.sakila.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-}
